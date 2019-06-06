1db07f9676afbcb2d9be7229e53247a69906d317
Ollivdh <olivier.vandenheede.mailbox@gmail.com>
2019-06-06 14:39:49 +0200

    Second part Olivier.

--- main.js
+++ main.js
@@ -2,10 +2,10 @@
 let pageNumber = 1;
 let all = `https://api.punkapi.com/v2/beers?page=${pageNumber}&per_page=80`;
 const random = 'https://api.punkapi.com/v2/beers/random';
-const displayRandom = document.getElementById("random");
-const displayOverview = document.getElementById("random");
+const displayRandom = document.getElementById("randomBeers");
 const displayAllBeers = document.getElementById('allbeers');
 const updateButton = document.getElementById('updatepage');
+const updateRandom = document.getElementById('updateRandom');
 
 updateButton.addEventListener("click", function(a){
   console.log("clicky");
@@ -18,15 +18,6 @@ updateButton.addEventListener("click", function(a){
   return pageNumber;
 });
 
-function displayRandomBeer(data) {
-    let randomBeer = data[0].name;
-    let randomImage = data[0].image_url;
-
-    // console.table(data);
-    displayRandom.innerHTML = randomBeer;
-    displayRandom.innerHTML += "<br>";
-    displayRandom.innerHTML += `<img src=${randomImage}>`;
-};
 
 function displayOverviewBeer(data){
     // let overviewBeer = data;
@@ -45,22 +36,29 @@ function displayOverviewBeer(data){
                                   `;
 }
 
-/*`<div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="${data[key].image_url}" alt="">
-  <div class="card-body">
-    <h5 class="card-title">${data[key].name}</h5>
-    <p class="card-text">S${data[key].tagline}</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>`*/
+
+function displayRandomBeer(data) {
+    let randomBeer = data[0].name;
+    let randomImage = data[0].image_url;
+
+    // console.table(data);
+    displayRandom.innerHTML = randomBeer;
+    displayRandom.innerHTML += "<br>";
+    displayRandom.innerHTML += `<img src=${randomImage}>`;
+};
+
 
 fetch(all)
     .then((resp) => resp.json())
     .then((data) => displayOverviewBeer(data));
 
 
+updateRandom.addEventListener("click", function(){
+    fetch(random)
+        .then((resp) => resp.json())
+        .then((data) => displayRandomBeer(data));
+    return;
+});
 
 
-/*fetch(random)
-    .then((resp) => resp.json())
-    .then((data) => displayRandomBeer(data));*/
+
