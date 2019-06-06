8398c86dc55e7c6703c7ceb66ff6387607764c6e
SimonRijsselaere <rijsselaere.simon@gmail.com>
2019-06-06 14:40:37 +0200

    update

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const displayOverview = document.getElementById("random");
 const displayAllBeers = document.getElementById('allbeers');
 const updateButton = document.getElementById('updatepage');
 
-updateButton.addEventListener("click", function(a){
+updateButton.addEventListener("click", function(){
   console.log("clicky");
   Number(pageNumber++);
   console.log(pageNumber);
