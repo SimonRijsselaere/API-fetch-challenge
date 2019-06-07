0ef084e6b1908627376d40547674855fe613c4c1
Ollivdh <olivier.vandenheede.mailbox@gmail.com>
2019-06-07 10:39:06 +0200

    Styling Optimized.

--- main.js
+++ main.js
@@ -29,6 +29,7 @@ nextButton.addEventListener("click", function() {
   return pageNumber;
 });
 
+// Sorts the page Alphabetic.
 sortPageButton.addEventListener("click", function() {
   function compare( a, b ) {
     if ( a.name < b.name ){
@@ -71,18 +72,47 @@ function displayOverviewBeer(data) {
                                       </div>`
                                   )).join(' ')}
                                   `;
-}
+};
 
 function displayRandomBeer(data) {
-  let randomBeer = data[0].name;
-  let randomKey = data[0].tagline;
-  let randomImage = data[0].image_url;
+  // let randomBeer = data[0].name;
+  // let randomKey = data[0].tagline;
+  // let randomImage = data[0].image_url;
+  //
+  // console.table(data);
+  // displayRandom.innerHTML = randomBeer;
+  // displayRandom.innerHTML += "<br>";
+  // displayRandom.innerHTML += randomKey;
+  // displayRandom.innerHTML += `<img src=${randomImage}>`;
 
-  console.table(data);
-  displayRandom.innerHTML = randomBeer;
-  displayRandom.innerHTML += "<br>";
-  displayRandom.innerHTML += randomKey;
-  displayRandom.innerHTML += `<img src=${randomImage}>`;
+  displayRandom.innerHTML =` 
+  
+  ${Object.keys(data).map(key => (
+                                    `<div id="cards" class="card d-inline-flex" style="width: 18rem;">
+                                    <h5 class="card-title">${data[key].id}</h5>
+  <img  class="beerImage card-img-top" src="${data[key].image_url}" alt="">
+      <div class="card-body">
+      <h5 class="card-title">${data[0].name}</h5>
+      <p class="card-text">${data[0].tagline}</p>
+      <a class="btn btn-primary" data-toggle="collapse" href="#${data[0].name}" role="button" aria-expanded="false" aria-controls="collapseExample">More info</a>
+  <div class="collapse mt-2" id="${data[0].name}">
+      <div class="card card-body">
+      <ul>
+      <li>alcohol level: ${data[0].abv}%</li>
+  <li>tip from the brewer: ${data[0].brewers_tips}</li>
+  <li>description: ${data[0].description}</li>
+  <li>PH-level: ${data[0].ph}</li>
+  <li>contributed by: ${data[0].contributed_by}</li>
+  </ul>
+  </div>
+  </div>
+  </div>
+  </div>`
+)).join(' ')}
+`;
+
+
+
 };
 
 fetch(all)
